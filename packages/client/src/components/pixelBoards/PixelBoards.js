4cb291b536642318a18601afafb4f7fc98e4b8e9
AyoubAdm <ayoubadm95@gmail.com>
2024-02-26 15:34:43 +0100

    mise en place des routes + creations des différents composants + mise en place du authContext

---
+++ packages/client/src/components/pixelBoards/PixelBoards.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PixelBoards() {
+  return (
+    <div>PixelBoards</div>
+  )
+}
---
+++ packages/client/src/contexts/AuthContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from 'react';
+
+
+//Contexte d'authentification qui permet de gérer l'état de connexion de l'utilisateur
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
