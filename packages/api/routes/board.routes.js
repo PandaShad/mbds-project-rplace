9179ff88edf4400e687946032b4553d43d5f2d4c
Maxime Bellet <maximebellet83@hotmail.fr>
2024-04-05 20:53:14 +0200

    <fix>(api): fixes for board routes

--- packages/api/routes/board.routes.js
+++ packages/api/routes/board.routes.js
@@ -49,21 +49,21 @@ boardRouter.get('/list-finished', async (_, res) => {
 	}
 });
 
-boardRouter.get('/:id', async (req, res) => {
-	try {
-		const board = await Board.findById(req.params.id);
-		return res.status(200).send(board);
-	} catch (error) {
-		return res.status(500).send(`There was a problem getting the board: ${error}`);
-	}
-});
-
 boardRouter.get('/count', async (_, res) => {
 	try {
 		const count = await Board.countDocuments();
 		return res.status(200).send({ count });
 	} catch (error) {
 		return res.status(500).send(`There was a problem counting the boards: ${error}`);
+	}
+});
+
+boardRouter.get('/:id', async (req, res) => {
+	try {
+		const board = await Board.findById(req.params.id);
+		return res.status(200).send(board);
+	} catch (error) {
+		return res.status(500).send(`There was a problem getting the board: ${error}`);
 	}
 });
 
@@ -76,18 +76,18 @@ boardRouter.put('/:id/update', async (req, res) => {
 		if (board.status === 'finished') {
 			return res.status(400).send('Board is already finished');
 		}
-		if (req.body.status === 'finished') {
+		if (req.body.status && req.body.status === 'finished') {
 			board.status = 'finished';
 			board.end_date = Date.now();
+		}
+		if (req.body.end_date && Date.parse(req.body.end_date) < Date.now()) {
+			return res.status(400).send('End date must be greater than current date');
 		}
 		board.title = req.body.title || board.title;
 		board.override = req.body.override || board.override;
 		board.description = req.body.description || board.description;
-		board.dimension = {
-			width: req.body.dimension.width || board.dimension.width,
-			height: req.body.dimension.height || board.dimension.height,
-		};
 		board.waiting_time = req.body.waiting_time || board.waiting_time;
+		board.end_date = req.body.end_date || board.end_date;
 		await board.save();
 		return res.status(200).send('Board updated');
 	} catch (error) {
