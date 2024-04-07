dd2f490efd145177edaa2eb77c41772a45d9103e
AyoubAdm <ayoubadm95@gmail.com>
2024-04-07 16:48:14 +0200

    fix(client): suppression code inutilisé

--- packages/client/src/services/userService.js
+++ packages/client/src/services/userService.js
@@ -9,21 +9,3 @@ export const fetchUserInfo = async () => {
 	});
 	return response.data;
 };
-
-export const fetchOngoingBoards = async () => {
-	const response = await axios.get(API_ROUTES.ongoingBoards, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
-	return response.data;
-};
-
-export const fetchFinishedBoards = async () => {
-	const response = await axios.get(API_ROUTES.finishedBoards, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
-	return response.data;
-};
